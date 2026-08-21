ae0bf464ee2493decfd80fac0bf30dfb78b68c29
Andreas Hygrell <andreas.hygrell@telia.com>
2026-08-21 15:24:50 +0200

    Fix typo

--- experiments/compare_braess_to_java/batch_run_all.sh
+++ experiments/compare_braess_to_java/batch_run_all.sh
@@ -10,7 +10,7 @@ source "${SCRIPT_DIR}/batch_common.sh"
 # Then run plotting averages over all seeds for every case.
 parse_common_args "$@"
 # run every experiment in rust, extract measurements and plot per seed
-run_for_each_replvar_variedseed_beta_seed_combo_parallel run_and_extract_and_plot_per_seed_casease
+run_for_each_replvar_variedseed_beta_seed_combo_parallel run_and_extract_and_plot_per_seed_case
 # try to extract measurements from the original java output events (will mostly fail because events are missing)
 run_for_each_replvar_variedseed_beta_seed_combo_parallel try_extracting_measurements_from_java_case
 # reformat the original java measurements into the same format as the rust measurements (because the above extraction mostly fails)
